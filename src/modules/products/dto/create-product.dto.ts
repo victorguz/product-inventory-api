a03c06e57f55c58c8a81049fd7a2567c8c4b6e1b
Victor Guzmán Berrio <victorguzber@gmail.com>
2024-06-24 12:15:59 -0500

    validaciones precio y cantidad

--- src/modules/products/dto/create-product.dto.ts
+++ src/modules/products/dto/create-product.dto.ts
@@ -1,25 +1,37 @@
-import { ApiProperty } from "@nestjs/swagger";
-import { IsInt, IsNotEmpty, IsNumber, IsOptional, IsString, Min } from "class-validator";
-
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsInt,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsPositive,
+  IsString,
+  Min,
+} from 'class-validator';
 
 export class CreateProductDto {
-    @IsString()
-    @IsNotEmpty()
-    @ApiProperty({ description: 'The name of the product', example: 'Product 1' })
-    nombre: string;
-  
-    @IsString()
-    @IsOptional()
-    @ApiProperty({ description: 'The description of the product', example: 'This is a product', required: false })
-    descripcion?: string;
-  
-    @IsNumber()
-    @Min(0)
-    @ApiProperty({ description: 'The price of the product', example: 9.99 })
-    precio: number;
-  
-    @IsInt()
-    @Min(0)
-    @ApiProperty({ description: 'The quantity of the product', example: 10 })
-    cantidad: number;
-  }
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({ description: 'The name of the product', example: 'Product 1' })
+  nombre: string;
+
+  @IsString()
+  @IsOptional()
+  @ApiProperty({
+    description: 'The description of the product',
+    example: 'This is a product',
+    required: false,
+  })
+  descripcion?: string;
+
+  @IsNumber()
+  @IsPositive()
+  @ApiProperty({ description: 'The price of the product', example: 9.99 })
+  precio: number;
+
+  @IsInt()
+  @IsPositive()
+  @Min(1)
+  @ApiProperty({ description: 'The quantity of the product', example: 10 })
+  cantidad: number;
+}
